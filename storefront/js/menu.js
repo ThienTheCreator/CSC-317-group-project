12ac1d059d7b11965528141831f96a929d1263ca
Shem Cheng <scheng@netsuite.com>
2021-04-13 05:00:59 -0700

    working buttons in menu

--- storefront/js/menu.js
+++ storefront/js/menu.js
@@ -1,19 +1,62 @@
-var addButtons = document.getElementsByClassName("button-add");
-var removeButtons = document.getElementsByClassName("button-minus");
-var foodQuant = document.getElementsByClassName("foodQuantity");
-
-for(var i=0; i<addButtons.length; i++){
-    var button = addButtons[i];
-    button.addEventListener('click', function(){
-        foodQuant[i].value = parseInt(input.value)+1;
-    })
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
 }
 
-for(var i=0; i<removeButtons.length; i++){
-    var button = removeButtons[i];
-    button.addEventListener('click', function(){
-        if(foodQuant[i].value>0){ // we don't want to let the amound order go less than 0
-            foodQuant[i].value = parseInt(foodQuant[i].value)-1;
-        }
-    })
+function ready(){
+    var addButtons = document.getElementsByClassName("button-add");
+    console.log(addButtons);
+
+    var removeButtons = document.getElementsByClassName("button-minus");
+    console.log(removeButtons);   
+
+    var foodQuant = document.getElementsByClassName("food-quantity");
+    console.log(foodQuant);
+
+    for (var i = 0; i < foodQuant.length; i++) {
+        var input = foodQuant[i];
+        input.addEventListener('change', quantityChanged);
+    }
+
+    for (var i = 0; i < addButtons.length; i++) {
+        var add_button = addButtons[i];
+        add_button.addEventListener('click', addCartItem);
+    }
+
+    for (var i = 0; i < removeButtons.length; i++) {
+        var rem_button = removeButtons[i];
+        rem_button.addEventListener('click', removeCartItem);
+    }
+
+}
+
+function quantityChanged(event){
+    console.log(event.target);
+    console.log("quant changed");
+}
+
+function addCartItem(event){
+    var button = event.target;
+    var food = button.parentElement;
+    var name = food.getElementsByClassName("shop-item-title")[0].innerText;
+    var price = food.getElementsByClassName("shop-item-price")[0].innerText;
+    var foodQuant = food.getElementsByClassName("food-quantity")[0];
+
+    foodQuant.value = parseInt(foodQuant.value)+1;
+}
+
+function removeCartItem(event){
+    var button = event.target;
+    var food = button.parentElement;
+    var name = food.getElementsByClassName("shop-item-title")[0].innerText;
+    var price = food.getElementsByClassName("shop-item-price")[0].innerText;
+    var foodQuant = food.getElementsByClassName("food-quantity")[0];
+
+    if(foodQuant.value > 0){
+        foodQuant.value = parseInt(foodQuant.value)-1;
+    } else {
+        console.log("quant is at minimum");
+    }
+    
 }
