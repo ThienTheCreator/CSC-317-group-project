d8471fd73dfda706d26e962dd34f5f73fe692c6c
Shem Cheng <scheng@netsuite.com>
2021-05-09 23:40:37 -0700

    fixed some links

--- storefront/js/account.js
+++ storefront/js/account.js
@@ -32,7 +32,7 @@ function check() {
         for (let u = 0; u < stored_users.length; u++){
             if (usrName == stored_users[u].name && usrPw == stored_users[u].password) {
                 alert('You are logged in ' + usrName);
-                try {return window.location = "../html/register.html";}
+                try {return window.location = "../index.html";}
                 catch(e) {console.log(e)}
              }
         }
